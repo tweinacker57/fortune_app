3acaa636593dc8ed6d7646bdc6bc851c293f34ee
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-02 21:44:51 -0500

    Added code for random fortune method that gives user a random fortune

--- app/controllers/api/fortunes_controller.rb
+++ app/controllers/api/fortunes_controller.rb
@@ -3,4 +3,9 @@ class Api::FortunesController < ApplicationController
     @message = "hello"
     render "sample.json.jb"
   end
+  def random_fortune
+    random_fortune = ["you will be happy", "you will have many children", "you will be alone"]
+    @fortune = random_fortune.sample
+    render 'fortune.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/sample_path" => "fortunes#sample_action"
+    get "/random_fortune" => "fortunes#random_fortune"
   end
 end
